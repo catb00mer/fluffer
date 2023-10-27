2910ab4cc41607512ea4e82a7abf17ba9c99aca9
catboomer <catb00mer@proton.me>
2023-10-26 22:48:56 -0500

    Added `errors` and `admin` examples.

---
+++ examples/admin.rs
@@ -0,0 +1,65 @@
+use fluffer::{async_trait, App, Context, Fluff, GemBytes};
+
+#[derive(thiserror::Error, Debug)]
+enum CustomErr {
+    #[error("Page not found.")]
+    Hide,
+}
+
+#[async_trait]
+impl GemBytes for CustomErr {
+    async fn gem_bytes(self) -> Vec<u8> {
+        let status = match self {
+            CustomErr::Hide => 51,
+        };
+
+        format!("{} {}\r\n", status, self).into_bytes()
+    }
+}
+
+// Uses CustomErr to hide pages if the user isn't admin.
+fn hide(ctx: &Context) -> Result<(), CustomErr> {
+    // Maybe read the certificate from a file instead.
+    let admin = "-----BEGIN CERTIFICATE-----
+MIICmTCCAYECCDxmaR4g0RXyMA0GCSqGSIb3DQEBCwUAMA4xDDAKBgNVBAMMA093
+TzAgFw0yMzEwMjUxNzIzMjdaGA85OTk5MDEwMTA2MDAwMFowDjEMMAoGA1UEAwwD
+T3dPMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnPgrliGdyfyZAWab
++QL338ztDVaLTdjhQTGAncRWrkZzmUbmKKHZBzSneQ3KyQxV745G2t/XrVxh1joB
+/HFBoVTKFZ4+mHbcXK8UcA/nLRRtmnr7fMI70i54jHmPqKKI7g++pY7u8b0GiW0q
+hyuFsfceqKb7PKeo57Cjq27OrU6GGyaIAQ+WKQche4uvXHY2pPdWnMnqXeA6kuxx
+KbKY9BEDURZrlxTJILfS6GuG628zFre0Bzg9R3JXrm6wXFEFsrc63VDt82SJW0XD
+5KDEgXzkE5NR2yh9FNyonDMcB6Z3JkJ1oZo5Ur52fdPEYxtljEJtXPG/ZhHtlVPt
+fwIVjQIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQAkOKIS9Z3s5pv2wtPmNONn2gxI
+JmI5/s0aCjvxcC58nBkhDoaniOfLPRfmior1PSYJO3CywsoVlLWNBPKidIKJaYcR
+cMlwTCvX8Yi71dGkAKXqogAE4R1bB5+mcF9fK5EN0LzCsKxh7CLLWIGDcz2xkBoS
+yfFa/hM29HqXhHvIVK0aJkn9J6DbV8UPGlasKk0mQswNNGT5mQMdKjXZGfsWkrkm
+I3JmHvLxq9osKGbA3jctThPIHr324AoWWENJf33lqs8/UVxu4DTDhRlmp9g900k0
+UDhrx+oupwUUcYnSaTR3gP44+IPU05mYLI6Pf3RiNP02u5ztpTpHS91nBNrx
+-----END CERTIFICATE-----";
+    if ctx.ident_verify(admin) {
+        Ok(())
+    } else {
+        Err(CustomErr::Hide)
+    }
+}
+
+async fn hidden_page(ctx: Context) -> Result<Fluff, CustomErr> {
+    hide(&ctx)?;
+
+    Ok(Fluff::Text("yo wasuup? 😎".to_string()))
+}
+
+#[tokio::main]
+async fn main() {
+    let app = App::default()
+        .route("/", |_| async {
+            "=> /hidden Absolutely nothing at this url btw"
+        })
+        .route("/hidden", hidden_page)
+        .run()
+        .await;
+
+    if let Err(e) = app {
+        eprintln!("Error: {e:?}");
+    }
+}
---
+++ examples/errors.rs
@@ -0,0 +1,38 @@
+use fluffer::{async_trait, App, Context, Fluff, GemBytes};
+
+#[derive(thiserror::Error, Debug)]
+enum CustomErr {
+    #[error("IO no worky :(")]
+    FileNoWorky(#[from] std::io::Error),
+}
+
+#[async_trait]
+impl GemBytes for CustomErr {
+    async fn gem_bytes(self) -> Vec<u8> {
+        // Decide which status is appropriate for each error
+        let status = match self {
+            _ => 40,
+        };
+
+        format!("{} {}\r\n", status, self).into_bytes()
+    }
+}
+
+async fn file(_: Context) -> Result<Fluff, CustomErr> {
+    let f = std::fs::read_to_string("./static/file.rs")?; // << gets converted into CustomErr
+
+    Ok(Fluff::Document {
+        mime: "text/rust".to_string(),
+        body: f,
+    })
+}
+
+#[tokio::main]
+async fn main() {
+    let app = App::default().route("/", file).run().await;
+
+    // << Print app errors to stderr instead of panicking
+    if let Err(e) = app {
+        eprintln!("Error: {e:?}");
+    }
+}
