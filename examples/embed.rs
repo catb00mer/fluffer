29c2386ff8872425820965918145b7ff71656b9b
catboomer <catb00mer@proton.me>
2023-11-05 03:07:50 -0600

    Updated `embed.rs` example with usage of `AsGemtext` trait

--- examples/embed.rs
+++ examples/embed.rs
@@ -1,49 +1,34 @@
-// Scenario: you have a type that implements GemBytes. It formats some displays some information that you want
-// to embed in a greater document.
+use fluffer::{App, AsGemtext, Client, Fluff, GemBytes};
 
-// 1. Call the desired route function.
-//
-// 2. Use the `ToGemtext` trait (implemented for `GemBytes`) to get the gemtext returned by the
-//    type (if any).
-
-use fluffer::{App, Fluff, ToGemtext};
-
-type Client = fluffer::Client<bool>; // ignore this :>
-
-async fn api(c: Client) -> Fluff {
-    // randomly returns one of these
-    if c.state {
+async fn api(_: Client) -> Fluff {
+    if rand::random::<bool>() {
         Fluff::Text("* ~ Some API information here!!!!!~".to_string())
     } else {
         Fluff::NotFound("This route is broked :(".to_string())
     }
 }
 
-async fn index(mut c: Client) -> Fluff {
-    c.state = rand::random::<bool>();
+async fn index(c: Client) -> Fluff {
     Fluff::Text(format!(
-        r#"# Welcome to this demo
-The stats below will fail to load 1/2 of the time. Keep refreshing.
+        r#"The stats below will fail to load 1/2 of the time. Keep refreshing.
 
-### -> to_gemtext_err()
-{}
-### -> to_gemtext().unwrap_or("api broken".to_string())
+## Routes within routes
+Here we call the api route, call gem_bytes() on it, and call as_gemtext() on the bytes.
+
+This allows us to embed the api route in this document.
+
+=> /api api
+
 {}"#,
-        api(c.clone()).await.to_gemtext_err().await,
-        api(c.clone())
-            .await
-            .to_gemtext()
-            .await
-            .unwrap_or("api broken".to_string()),
+        api(c).await.gem_bytes().await.as_gemtext()
     ))
 }
 
 #[tokio::main]
 async fn main() {
     App::default()
-        .state(true)
         .route("/", index)
-        .route("/api", api) // <- it's not neccessary to make the /api route public, since we call the function directly
+        .route("/api", api)
         .run()
         .await
         .unwrap()
