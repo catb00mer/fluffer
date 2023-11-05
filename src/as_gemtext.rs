2a218ff672621ec6e83d13dbbca73efa790a1fae
catboomer <catb00mer@proton.me>
2023-11-05 03:04:20 -0600

    `ToGemtext` -> `AsGemtext`; implemented it on Vec<u8> instead so it can be used for other things.

---
+++ src/as_gemtext.rs
@@ -0,0 +1,34 @@
+use url::Url;
+
+/// 📝 Represent type as gemtext.
+pub trait AsGemtext {
+    fn as_gemtext(&self) -> String;
+}
+
+impl AsGemtext for Vec<u8> {
+    /// Implementation to parse gemini bytes for gemtext.
+    ///
+    /// If the bytes begin with anything other than `20
+    /// text/gemini...`, then the status is returned inside
+    /// a code block.
+    fn as_gemtext(&self) -> String {
+        if let Ok(b) = std::str::from_utf8(&self) {
+            if let Some((header, content)) = b.split_once("\r\n") {
+                if let Some(pos) = header.find("20 text/gemini") {
+                    if pos == 0 {
+                        return content.to_string();
+                    }
+                }
+                return format!("```\n{header}\n```");
+            }
+            return String::from("```\ninvalid gemini response\n```");
+        }
+        String::from("```\ninvalid utf8\n```")
+    }
+}
+
+impl AsGemtext for Url {
+    fn as_gemtext(&self) -> String {
+        format!("=> {} {}", self, self)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 extern crate log;
 
 mod app;
+mod as_gemtext;
 mod client;
 mod err;
 mod fluff;
@@ -13,10 +14,11 @@ mod gem_call;
 mod interactive;
 
 pub use app::App;
+pub use as_gemtext::AsGemtext;
 pub use client::Client;
 pub use err::AppErr;
 pub use fluff::Fluff;
-pub use gem_bytes::{GemBytes, ToGemtext};
+pub use gem_bytes::GemBytes;
 pub use gem_call::GemCall;
 
 /// Procedural macro that must be used in implementations of [`GemBytes`].
